adaa62972138c2cc6a9a97c6eed1126a112d8d1f
ramil-kham <ramil66-2010@mail.ru>
2021-02-18 21:55:18 +0300

    Print body mass index

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int weight = 65;
+        double height = 1.74 * 1.74;
+        double index = weight / height;
+        System.out.println(index);
+    }
+}
