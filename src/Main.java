8e6b6a440eba62a3bcbd3fc6042d1e679b8b3116
ramil-kham <ramil66-2010@mail.ru>
2021-02-21 21:35:18 +0300

    Исправил типы данных

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int weight = 65;
-        double height = 174;
-        double index = weight / (height * height / 10_000);
+        int height = 174;
+        int index = weight / (height * height / 10_000);
         System.out.println(index);
     }
 }
