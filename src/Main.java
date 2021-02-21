94438ff9dea5f18baa0838d0bc805e7647e080dd
ramil-kham <ramil66-2010@mail.ru>
2021-02-21 19:22:35 +0300

    Исправил замечания

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int weight = 65;
-        double height = 1.74 * 1.74;
-        double index = weight / height;
+        double height = 174;
+        double index = weight / (height * height / 10_000);
         System.out.println(index);
     }
 }
